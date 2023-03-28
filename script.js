9998a8469b1e3655347a028be9a675133f5e6199
vitorher <vitorherpereira@gmail.com>
2023-03-28 19:58:38 -0300

    feat chat

---
+++ script.js
@@ -0,0 +1,30 @@
+alert("Teste");
+
+function adicionaMenssagem(textoMenssage) {
+    const novaMsg = document.createElement("div");
+    novaMsg.innerHTML = `<div class="item2">
+                            <div class="user"><strong>User diz:</strong></div>
+                            <div class="chat">${textoMenssage}</div>
+                          </div>`;
+
+    const menssagens = document.querySelector("#messages-list");
+    menssagens.appendChild(novaMsg);
+}
+  
+  function submitAdicionar(event) {
+    event.preventDefault();
+    
+    const inputMsg = document.querySelector("#message");
+    
+    if(inputMsg.value === "") {
+      alert("Insira uma menssagem valida!");
+      return;
+    }
+    
+    adicionaMenssagem(inputMsg.value);
+    
+    inputMsg.value = "";
+  }
+  
+  const formAdd = document.querySelector("#form-chat-send");
+  formAdd.addEventListener("submit", submitAdicionar);
