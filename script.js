12529bd14616d2bd330742dde73756ebce4b3149
vitorhenriquepereira <vitorherpereira@gmail.com>
2023-03-28 21:28:17 -0300

    fix chat

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-alert("Teste");
+function posicionaFim() {
+  const scrollChat = document.querySelector("#messages-list");
+  scrollChat.scrollTop = scrollChat.scrollHeight;
+}
+
+posicionaFim();
 
 function adicionaMenssagem(textoMenssage) {
-    const novaMsg = document.createElement("div");
-    novaMsg.innerHTML = `<div class="item2">
-                            <div class="user"><strong>User diz:</strong></div>
-                            <div class="chat">${textoMenssage}</div>
-                          </div>`;
+  const novaMsg = document.createElement("div");
+  novaMsg.innerHTML = `<div class="item2">
+                        <div class="user"><strong>User diz:</strong></div>
+                        <div class="chat">${textoMenssage}</div>
+                      </div>`;
 
-    const menssagens = document.querySelector("#messages-list");
-    menssagens.appendChild(novaMsg);
+  const menssagens = document.querySelector("#messages-list");
+  menssagens.appendChild(novaMsg);
+
+  posicionaFim();
 }
-  
-  function submitAdicionar(event) {
-    event.preventDefault();
-    
-    const inputMsg = document.querySelector("#message");
-    
-    if(inputMsg.value === "") {
-      alert("Insira uma menssagem valida!");
-      return;
-    }
-    
-    adicionaMenssagem(inputMsg.value);
-    
-    inputMsg.value = "";
+
+function submitAdicionar(event) {
+  event.preventDefault();
+
+  const inputMsg = document.querySelector("#message");
+
+  if (inputMsg.value === "") {
+    alert("Insira uma menssagem valida!");
+    return;
   }
-  
-  const formAdd = document.querySelector("#form-chat-send");
-  formAdd.addEventListener("submit", submitAdicionar);
+
+  adicionaMenssagem(inputMsg.value);
+
+  inputMsg.value = "";
+}
+
+const formAdd = document.querySelector("#form-chat-send");
+formAdd.addEventListener("submit", submitAdicionar);
